a2308669901f572ee388ca447cb4117381897319
Naouh66x <nouun69@gmail.com>
2025-07-15 01:15:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log('🎲 Serveur lancé sur http://localhost:' + PORT);
 });
